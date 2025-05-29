51808c64cb57370b4302491b0af0991ec0ee22a5
Sabi <numanovas@yandex.ru>
2025-05-29 18:37:28 +0300

    Задание 2.6

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,25 +1,14 @@
 package org.example;
 
-import static org.example.Task.*;
+import static org.example.StudentManager.*;
+import static org.example.PhoneBook.*;
+
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
-        printThreeWords();
-        checkSumSign();
-        printColor();
-        compareNumbers();
-        checkSumInRange();
-        printNumberSign();
-        isNegative();
-        printStringMultipleTimes();
-        isLeapYear();
-        printArrayOne();
-        printArrayTwo();
-        printArrayThree();
-        printArrayFour();
-        printArrayFive();
+        execute();
+        execution();
     }
 }
--- src/main/java/org/example/Task.java
+++ src/main/java/org/example/Task.java
@@ -1,197 +1,115 @@
 package org.example;
+import java.util.*;
 
-public class Task {
 
-    public static void printThreeWords() {
-        System.out.println("Orange");
-        System.out.println("Banana");
-        System.out.println("Apple");
-    }
+    class Student {
+        private String name;
+        private String group;
+        private int course;
+        private List<Integer> grades;
 
-    public static void checkSumSign() {
-        int a = 19;
-        int b = 8;
-        int sum = a + b;
-        if (sum >= 0) {
-            System.out.println("Сумма положительная");
-        } else {
-            System.out.println("Сумма отрицательная");
+        public Student(String name, String group, int course, List<Integer> grades) {
+            this.name = name;
+            this.group = group;
+            this.course = course;
+            this.grades = new ArrayList<>(grades);
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getCourse() {
+            return course;
+        }
+
+        public void nextCourse() {
+            this.course++;
+        }
+
+        public double averageGrade() {
+            if (grades.isEmpty()) return 0;
+            double sum = 0;
+            for (int grade : grades) {
+                sum += grade;
+            }
+            return sum / grades.size();
+        }
+
+        public String toString() {
+            return name + " (группа: " + group + ", курс: " + course + ", средний балл: " + averageGrade() + ")";
         }
     }
 
-    public static void printColor() {
-        int value = 19;
-        if (value <= 0) {
-            System.out.println("Красный");
-        } else if (value > 0 && value <= 100) {
-            System.out.println("Желтый");
-        } else {
-            System.out.println("Зеленый");
+     class StudentManager {
+
+        public static void removeLowGrades(Set<Student> students) {
+            students.removeIf(student -> student.averageGrade() < 3);
         }
-    }
 
-    public static void compareNumbers() {
-        int a = 19;
-        int b = 8;
-        if (a >= b) {
-            System.out.println("a >= b");
-        } else {
-            System.out.println("a < b");
-        }
-    }
-
-    public static void checkSumInRange() {
-        System.out.println(checkSumInRange(18, 1));
-        System.out.println(checkSumInRange(1, 19));
-        System.out.println(checkSumInRange(20, 1));
-    }
-
-    public static boolean checkSumInRange(int a, int b) {
-        int sum = a + b;
-        return sum >= 10 && sum <= 20;
-    }
-
-    public static void printNumberSign() {
-        printNumberSign(19);
-        printNumberSign(0);
-        printNumberSign(-8);
-    }
-
-    public static void printNumberSign(int number) {
-        if (number >= 0) {
-            System.out.println("Положительное");
-        } else {
-            System.out.println("Отрицательное");
-        }
-    }
-
-    public static void isNegative() {
-        System.out.println(isNegative(1));
-        System.out.println(isNegative(0));
-        System.out.println(isNegative(-1));
-    }
-
-    public static boolean isNegative(int number) {
-        return number < 0;
-    }
-
-    public static void printStringMultipleTimes() {
-        printStringMultipleTimes("Солнце восходит на востоке", 3);
-        printStringMultipleTimes("Лес растворился в темноте", 2);
-    }
-
-    public static void printStringMultipleTimes(String str, int count) {
-        for (int i = 0; i < count; i++) {
-            System.out.println(str);
-        }
-    }
-
-    public static void isLeapYear()  {
-        System.out.println(isLeapYear(2025));
-        System.out.println(isLeapYear(1998));
-        System.out.println(isLeapYear(2000));
-        System.out.println(isLeapYear(1600));
-    }
-
-    public static boolean isLeapYear(int year) {
-        return (year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0));
-    }
-
-    public static void printArrayOne() {
-        int[] arrayOne = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
-        System.out.println("Исходный массив:");
-        printArrayOne(arrayOne);
-        for (int i = 0; i < arrayOne.length; i++) {
-            if (arrayOne[i] == 0) {
-                arrayOne[i] = 1;
-            } else {
-                arrayOne[i] = 0;
+        public static void promoteStudents(Set<Student> students) {
+            for (Student student : students) {
+                if (student.averageGrade() >= 3) {
+                    student.nextCourse();
+                }
             }
         }
-        System.out.println("Измененный массив:");
-        printArrayOne(arrayOne);
-    }
 
-    public static void printArrayOne(int[] arrayOne) {
-        for (int num : arrayOne) {
-            System.out.print(num + " ");
-        }
-        System.out.println();
-    }
-
-    public static void printArrayTwo()  {
-        int[] arrayTwo = new int[100];
-        for (int i = 0; i < arrayTwo.length; i++) {
-            arrayTwo[i] = i + 1;
-        }
-        System.out.println("Заполненный массив:");
-        printArrayTwo(arrayTwo);
-    }
-
-    public static void printArrayTwo(int[] arrayTwo) {
-        for (int num : arrayTwo) {
-            System.out.print(num + " ");
-        }
-        System.out.println();
-    }
-
-    public static void printArrayThree()  {
-        int[] arrayThree = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-        for (int i = 0; i < arrayThree.length; i++) {
-            if (arrayThree[i] < 6) {
-                arrayThree[i] *= 2;
+        public static void printStudents(Set<Student> students, int course) {
+            System.out.println("Студенты курса " + course + ":");
+            for (Student student : students) {
+                if (student.getCourse() == course) {
+                    System.out.println(student.getName());
+                }
             }
         }
-        System.out.println("Измененный массив:");
-        printArrayThree(arrayThree);
-    }
 
-    public static void printArrayThree(int[] arrayThree) {
-        for (int num : arrayThree) {
-            System.out.print(num + " ");
-        }
-        System.out.println();
-    }
+        public static void execute() {
+            Set<Student> students = new HashSet<>();
 
-    public static void printArrayFour() {
-        int size = 5;
-        int[][] arrayFour = new int[size][size];
-        for (int i = 0; i < size; i++) {
-            arrayFour[i][i] = 1;
-        }
-        System.out.println("Квадратный двумерный массив с единицами на главной диагонали:");
-        printArrayFour(arrayFour);
-    }
+            students.add(new Student("Обухова Мария", "Группа1", 1, Arrays.asList(5, 5, 4)));
+            students.add(new Student("Артимович Анжелика", "Группа2", 2, Arrays.asList(2, 2, 3)));
+            students.add(new Student("Тимошина Карина", "Группа1", 1, Arrays.asList(3, 3, 3)));
+            students.add(new Student("Попоян Александр", "Группа3", 3, Arrays.asList(2, 3, 3)));
 
-    public static void printArrayFour(int[][] arrayFour) {
-        for (int[] row : arrayFour) {
-            for (int num : row) {
-                System.out.print(num + " ");
-            }
-            System.out.println();
+            System.out.println("Все студенты:");
+            students.forEach(System.out::println);
+
+            removeLowGrades(students);
+            System.out.println("\nПосле удаления студентов со средним баллом < 3:");
+            students.forEach(System.out::println);
+
+            promoteStudents(students);
+            System.out.println("\nПосле перевода на следующий курс:");
+            students.forEach(System.out::println);
+
+            printStudents(students, 2);
         }
     }
 
-    public static void printArrayFive() {
-        int len = 8;
-        int initialValue = 19;
-        int[] resultArray = createArray(len, initialValue);
-        System.out.println("Созданный массив:");
-        printArrayFive(resultArray);
+    class PhoneBook {
+        private Map<String, List<String>> phoneBook = new HashMap<>();
+
+        public void add(String lastName, String phoneNumber) {
+            phoneBook.computeIfAbsent(lastName, k -> new ArrayList<>()).add(phoneNumber);
+        }
+
+        public List<String> get(String lastName) {
+            return phoneBook.getOrDefault(lastName, Collections.emptyList());
+        }
+
+        public static void execution() {
+            PhoneBook pb = new PhoneBook();
+
+            pb.add("Скачкова", "+7-999-465-95-06");
+            pb.add("Скачкова", "+7-999-999-99-99");
+            pb.add("Попоян", "+7-921-111-11-11");
+
+            System.out.println("Телефоны Скачковой: " + pb.get("Скачкова"));
+            System.out.println("Телефоны Попояна: " + pb.get("Попоян"));
+        }
     }
 
-    public static int[] createArray(int len, int initialValue) {
-        int[] array = new int[len];
-        for (int i = 0; i < len; i++) {
-            array[i] = initialValue;
-        }
-        return array;
-    }
 
-    public static void printArrayFive(int[] arrayFive) {
-        for (int num : arrayFive) {
-            System.out.print(num + " ");
-        }
-        System.out.println();
-    }
-}
+
+
