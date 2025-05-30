daa1b84b8ca66bf33e42cce9bf6121620d0f4ab5
Sabi <numanovas@yandex.ru>
2025-05-30 20:38:38 +0300

    Задание 2.3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,13 @@
 package org.example;
 
 import static org.example.Product.*;
+import static org.example.Park.*;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         product();
+        park();
     }
 }
--- src/main/java/org/example/Product.java
+++ src/main/java/org/example/Product.java
@@ -3,8 +3,6 @@ package org.example;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Product {
     private String name;
@@ -58,59 +56,6 @@ public class Product {
         for (Product product : productsArray) {
             product.printInfo();
             System.out.println("---------------------------");
-        }
-    }
-
-    public static class Park {
-        private String name;
-        private Attraction[] attractions;
-        private int attractionCount;
-
-        public Park(String name, int maxAttractions) {
-            this.name = name;
-            this.attractions = new Attraction[maxAttractions];
-            this.attractionCount = 0;
-        }
-
-        public void addAttraction(String name, String workingHours, double price) {
-            if (attractionCount < attractions.length) {
-                attractions[attractionCount++] = new Attraction(name, workingHours, price);
-            } else {
-                System.out.println("Достигнуто максимальное количество аттракционов.");
-            }
-        }
-
-        public void displayAttractions() {
-            System.out.println("Аттракционы в парке " + name + ":");
-            for (int i = 0; i < attractionCount; i++) {
-                System.out.println(attractions[i]);
-            }
-        }
-
-        private class Attraction {
-            private String name;
-            private String workingHours;
-            private double price;
-
-            public Attraction(String name, String workingHours, double price) {
-                this.name = name;
-                this.workingHours = workingHours;
-                this.price = price;
-            }
-
-            public String toString() {
-                return "Аттракцион: " + name + ", Время работы: " + workingHours + ", Стоимость: " + price + " руб.";
-            }
-        }
-
-        public static void park() {
-                Park park = new Park("Диво Остров",4);
-
-                park.addAttraction("Вокруг света", "11:00 - 21:00", 800);
-                park.addAttraction("Картинг", "11:00 - 21:00", 3000);
-                park.addAttraction("Катамараны", "12:00 - 29:00", 550);
-
-                park.displayAttractions();
         }
     }
 }
