58eb2899eb7235532bacb46dd7460ded14119d36
Sabi <numanovas@yandex.ru>
2025-06-13 22:05:43 +0300

    Lesson2.9

--- src/test/java/MtsTest.java
+++ src/test/java/MtsTest.java
@@ -15,7 +15,7 @@ import java.util.stream.Collectors;
 import static org.junit.jupiter.api.Assertions.*;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-public class MtsOnlineTopUpTest {
+public class MtsTest {
 
     private WebDriver driver;
 
